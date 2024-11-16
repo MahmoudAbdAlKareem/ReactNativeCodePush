33a3b24912f425fbbf1c6e54c87e6212136fb7a1
mahmoudabdalkarim <mabdalkareem@restaurant365.com>
2024-11-16 19:55:47 +0200

    Load JS Bundle Dynamically For Android

--- App.tsx
+++ App.tsx
@@ -77,7 +77,7 @@ function App(): React.JSX.Element {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Section title="Step One">
-           Mahmoud Custom Bundle <Text style={styles.highlight}>App.tsx</Text> to change this
+           App <Text style={styles.highlight}>App.tsx</Text> to change this
             screen and then come back to see your edits.
           </Section>
           <Section title="See Your Changes">
