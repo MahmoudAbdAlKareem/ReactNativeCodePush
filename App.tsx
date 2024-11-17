71716e63d09427c7f8bc3bc4addabee5afa88a8e
mahmoudabdalkarim <mabdalkareem@restaurant365.com>
2024-11-17 15:02:47 +0200

    Fetch and download the bundle automatically

--- App.tsx
+++ App.tsx
@@ -77,7 +77,7 @@ function App(): React.JSX.Element {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Section title="Step One">
-           Try Release Branch <Text style={styles.highlight}>App.tsx</Text> to change this
+           Mahmoud First Release <Text style={styles.highlight}>App.tsx</Text> to change this
             screen and then come back to see your edits.
           </Section>
           <Section title="See Your Changes">
