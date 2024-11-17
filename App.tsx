6c540e41eeef760eea167b40f695eaabc0a75a2b
mahmoudabdalkarim <mabdalkareem@restaurant365.com>
2024-11-17 12:39:30 +0200

    Test bundle file

--- App.tsx
+++ App.tsx
@@ -77,7 +77,7 @@ function App(): React.JSX.Element {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Section title="Step One">
-           App <Text style={styles.highlight}>App.tsx</Text> to change this
+           Try Release Branch <Text style={styles.highlight}>App.tsx</Text> to change this
             screen and then come back to see your edits.
           </Section>
           <Section title="See Your Changes">
