7844153e62bcdb454eead486a43f6b17758a9396
mahmoudabdalkarim <mabdalkareem@restaurant365.com>
2024-11-17 18:07:36 +0200

    Release new code

--- App.tsx
+++ App.tsx
@@ -76,7 +76,7 @@ function App(): React.JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
+          <Section title="New Release">
            Mahmoud Test Release 2 <Text style={styles.highlight}>App.tsx</Text> to change this
             screen and then come back to see your edits.
           </Section>
